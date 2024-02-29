9c8329e5b84a00810649761c962ea8c502b66143
kaito01234 <43839796+kaito01234@users.noreply.github.com>
2024-02-29 02:22:00 +0000

    update modules

--- bin/typescript-node.ts
+++ bin/typescript-node.ts
@@ -4,9 +4,10 @@ import * as cdk from 'aws-cdk-lib';
 import { TypescriptNodeStack } from '../lib/typescript-node-stack';
 
 const app = new cdk.App();
-new TypescriptNodeStack(app, 'TypescriptNodeStack', {
+const stack = new TypescriptNodeStack(app, 'TypescriptNodeStack', {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: 'ap-northeast-1',
   },
 });
+cdk.Tags.of(stack).add('tag-key', 'make-cdk');
